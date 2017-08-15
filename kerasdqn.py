5046c4faaf9cd89d12f8282fcca3fc8bbd5331e1
Shresth <vermashresth@gmail.com>
2017-08-15 23:33:11 +0530

    kerasdqn

--- kerasdqn.py
+++ kerasdqn.py
@@ -70,3 +70,19 @@ if __name__="__main__":
     agent=DQNAgent(state_size,action_size)
     done=False
     batch_size=32
+    
+    for e in range(EPISODES):
+        state=env.reset()
+        state=np.reshape(state,[1,state_size])
+        for time in range(500):
+            action=agent.act(state)
+            next_state,reward,done,_=env.step(action)
+            reward =reward if not done else -10
+            next_state=np.reshape(next_state,[1,state_size])
+            agent.remember(state,action,reward,next_state,done)
+            state=next_state
+            if done:
+                print("episode: {}/{},score: {}, e: {:.2}".format(e,EPISODES,time,agent.epsilon)
+                break
+        if len(agent.memory>batch_size):
+            agent.replay(batch_size)
